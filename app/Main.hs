13dcc851076a110bde911ee9c6d642b7130367cf
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-04 22:24:54 +1200

    Added game loop to main

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,13 @@ module Main where
 
 import Snake
 
+loop :: State -> IO ()
+loop state = do
+    putStr $ show state
+    move <- getLine
+    loop $ nextState (registerMove state (read move :: Direction))
+
 main :: IO ()
-main = undefined
+main = do
+    putStrLn "Starting snake game..."
+    loop defaultState
