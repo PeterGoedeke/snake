742b42fbd283692fa37fe0d689bd1dafed179725
PeterGoedeke <peterbgoedeke@gmail.com>
2020-08-05 09:32:26 +1200

    Setup main to allow the game to be played

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,20 @@
 module Main where
 
 import Snake
+import System.Random
 
-loop :: State -> IO ()
-loop state = do
+loop :: State -> [Point] -> IO ()
+loop state (x:xs) = do
     putStr $ show state
     move <- getLine
-    loop $ nextState (registerMove state (read move :: Direction))
+    loop (nextState (registerMove state (read move :: Direction)) x) xs
+loop _ invalidRandoms = undefined
 
 main :: IO ()
 main = do
+    gen <- getStdGen
+    let (genW, genH) = split gen
+        randomWidths = randomRs (0, width defaultState) genW
+        randomHeights = randomRs (0, height defaultState) genH
     putStrLn "Starting snake game..."
-    loop defaultState
+    loop defaultState (zip randomWidths randomHeights)
